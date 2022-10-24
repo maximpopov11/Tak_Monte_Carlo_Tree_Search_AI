b39e18202f22b139e79c05b48edf3483391592cc
maximpopov11 <icecreamguy0@gmail.com>
2022-10-24 11:31:40 -0500

    Implement Tak state initialization

--- Tak.py
+++ Tak.py
@@ -1,16 +1,21 @@
+from collections import deque, namedtuple
+
+GameState = namedtuple('GameState', 'to_move, board, pieces, moves')
+
+
 class Tak:
     """Tak game object which controls all game aspects."""
 
-    def __init__(self, board_length):
-        """Sets the initial state."""
-        self.board_length = board_length
-        self.__initialize_board()
-        self.__initialize_initial_state()
-
-    def __initialize_board(self):
-        """Initializes the board."""
-        pass
-
-    def __initialize_initial_state(self):
-        """Initializes non-board state."""
-        pass
+    def __init__(self, board_length, num_stones, num_capstones):
+        """Set the initial game state."""
+        board = [board_length][board_length]
+        for i in range(0, board_length):
+            for j in range(0, board_length):
+                board[i][j] = deque()
+        self.white_pieces = [num_stones, num_capstones]
+        self.black_pieces = [num_stones, num_capstones]
+        self.placements = [(x, y) for x in range(0, board_length) for y in range(0, board_length)]
+        self.white_moves = []
+        self.black_moves = []
+        moves = self.placements + self.white_moves
+        self.initial = GameState(to_move='W', board=board, pieces=self.white_pieces, moves=moves)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Tak import Tak
 
 BOARD_LENGTH = 5
+NUM_STONES = 21
+NUM_CAPSTONES = 1
 
-game = Tak(BOARD_LENGTH)
+game = Tak(BOARD_LENGTH, NUM_STONES, NUM_CAPSTONES)
